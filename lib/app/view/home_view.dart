ff551ae3c1532627220a4472682aa0124c55480c
Rodrigo Ditrich <rodrigo.jonas.dittrich@gmail.com>
2021-08-31 14:02:45 -0300

    #1 - Initial

---
+++ lib/app/view/home_view.dart
@@ -0,0 +1,23 @@
+import 'package:commons/commons.dart';
+import 'package:commons_dependencies/commons_dependencies.dart';
+import 'package:flutter/material.dart';
+
+class HomeView  extends StatelessWidget {
+  const HomeView({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBarWidget.getAppBar(title: "Home View"),
+      body: Stack(
+        children: [
+          HeaderWidget(allowLogout: true, function: _logout)
+        ],
+      ),      
+    );
+  }
+
+  void _logout() {
+    Modular.to.navigate('/login');
+  }
+}
